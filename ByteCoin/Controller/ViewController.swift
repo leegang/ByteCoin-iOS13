535ad3b98f295499cf98035c77a0721835a3ee3f
zuogang.li <bestmrlee@gmail.com>
2019-11-03 01:18:16 +0800

    Linked the Ui to code

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let coinManager = CoinManager()
 
+    @IBOutlet weak var currencyPikerView: UIPickerView!
+    @IBOutlet weak var bicoinLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPikerView.dataSource = self
+        currencyPikerView.delegate = self
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension  ViewController:UIPickerViewDataSource,UIPickerViewDelegate{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        unitLabel.text = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for:coinManager.currencyArray[row])
+    }
+    
 }
 
